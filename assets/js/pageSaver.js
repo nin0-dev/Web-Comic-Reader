4524b881ae849e0d71f0d3bdfb04cbf33416751b
nin0dev <serviceclient3dmart@gmail.com>
2022-08-21 12:08:38 -0400

    Added page saving

--- assets/js/pageSaver.js
+++ assets/js/pageSaver.js
@@ -1,4 +1,50 @@
+
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+	const d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	let expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+
 setInterval(function(){
-	const urlParams = new URLSearchParams(window.location.search);
-	console.log(urlParams.get('slide'))
+	var url = window.location.href
+	url = url.replace("#", "?")
+	const urlParams = new URLSearchParams(url);
+	if(urlParams.get('slide') == null)
+	{
+		return -1;
+	}
+	setCookie("currentPage", urlParams.get('slide'), 1000)
+	setCookie("currentBook", document.getElementById("file-path").value, 1000)
 }, 1);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {
+      // specify options here
+    });
+  });
+
+function restorePage()
+{
+	document.getElementsByClassName("imgUrl")[0].click()
+	window.location.href = "#lg=1&slide=" + getCookie("currentPage")
+}
